18d689c999d05e9e5f6f3aa2caf548a53e566cb3
Masaya Heywood <mkh429@nyu.edu>
2020-09-27 16:31:19 -0400

    Finished Warps, working on enemies

---
+++ Assets/enemyWarper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyWarper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 6f;
+    public Vector2 playerPosition = new Vector2(0, 0);
+    public GameObject playerCharacter;
+    public GameObject aggroRange;
+    public warpSpawn spawn;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
+    }
+}
--- Assets/warpSpawn.cs
+++ Assets/warpSpawn.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class warpSpawn : MonoBehaviour
 {
+    public GameObject playerCharacter;
+    public bool warp = false;
     // Start is called before the first frame update
     void Start()
     {
+        //playerCharacter = GameObject.Find("obj_PlayerCharacter");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (warp == true)
+        {
+            playerCharacter.transform.position = transform.position;
+            warp = false;
+        }
     }
 }
