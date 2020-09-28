99ad0dcb0e9746c40b1368fe1e85bd21a553a092
Masaya Heywood <mkh429@nyu.edu>
2020-09-27 23:53:03 -0400

    Minor Changes

--- Assets/ControllerScript.cs
+++ Assets/ControllerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControllerScript : MonoBehaviour
 {
     public int trophyScore = 0;
+    public warpSpawn spawn;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,9 @@ public class ControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            spawn.warp = true;
+        }
     }
 }
