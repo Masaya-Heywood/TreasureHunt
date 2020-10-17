e2c5fcc33af5a5858b739fb30c32b05357e83ee1
Masaya Heywood <mkh429@nyu.edu>
2020-10-16 23:36:42 -0400

    Finished A*
    Complete A* movement with functioning aggro range

--- Assets/Scripts/A Star Pathing/Node.cs
+++ Assets/Scripts/A Star Pathing/Node.cs
@@ -2,27 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+
+//credit to Sebastian Lauge 
+public class Node : IHeapItem<Node>
 {
-    public bool walkable;
-    public Vector2 worldPos;
-    public int gridX;
-    public int gridY;
 
-    public int gCost;
-    public int hCost;
-    public Node parent;
+	public bool walkable;
+	public Vector2 worldPos;
+	public int gridX;
+	public int gridY;
 
-    public Node(bool assignWalkable, Vector3 assignWorldPos)
-    {
-        walkable = assignWalkable;
-        worldPos = assignWorldPos;
-    }
+	public int gCost;
+	public int hCost;
+	public Node parent;
+	int heapIndex;
 
-    public int fCost { 
-        get
-        {
-            return gCost + hCost;
-        }
-    }
+	public Node(bool _walkable, Vector2 _worldPos, int _gridX, int _gridY)
+	{
+		walkable = _walkable;
+		worldPos = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+	}
+
+	public int fCost
+	{
+		get
+		{
+			return gCost + hCost;
+		}
+	}
+
+	public int HeapIndex
+	{
+		get
+		{
+			return heapIndex;
+		}
+		set
+		{
+			heapIndex = value;
+		}
+	}
+
+	public int CompareTo(Node nodeToCompare)
+	{
+		int compare = fCost.CompareTo(nodeToCompare.fCost);
+		if (compare == 0)
+		{
+			compare = hCost.CompareTo(nodeToCompare.hCost);
+		}
+		return -compare;
+	}
 }
---
+++ Assets/Scripts/A Star Pathing/Unit.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Dynamic;
+using UnityEngine;
+
+
+//credit to Sebastian Lauge 
+public class Unit : MonoBehaviour
+{
+
+	public Transform target;
+	public float speed = 20;
+	public int aggroThreshold = 10;
+	private Rigidbody2D rb2D;
+
+
+	Vector2[] path;
+	int targetIndex;
+	int dist;
+
+	void Start()
+	{
+		StartCoroutine(RefreshPath());
+		rb2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		dist = Mathf.RoundToInt(Vector2.Distance((Vector2)target.position, (Vector2)transform.position));
+	}
+
+	IEnumerator RefreshPath()
+	{
+		Vector2 targetPositionOld = (Vector2)target.position + Vector2.up; // ensure != to target.position initially
+
+		while (true)
+		{
+			if (targetPositionOld != (Vector2)target.position)
+			{
+				targetPositionOld = (Vector2)target.position;
+
+				path = Pathfinding.RequestPath(transform.position, target.position);
+				StopCoroutine("FollowPath");
+				StartCoroutine("FollowPath");
+			}
+
+			yield return new WaitForSeconds(.25f);
+		}
+	}
+
+	IEnumerator FollowPath()
+	{
+		if (path.Length > 0 && dist <= aggroThreshold && path.Length < 3)
+		{
+			targetIndex = 0;
+			Vector2 currentWaypoint = path[0];
+
+			while (true)
+			{
+				if ((Vector2)transform.position == currentWaypoint)
+				{
+					targetIndex++;
+					if (targetIndex >= path.Length)
+					{
+						yield break;
+					}
+					currentWaypoint = path[targetIndex];
+				}
+
+				//transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+
+				Vector2 pathDirection = (currentWaypoint - (Vector2)transform.position).normalized;
+				rb2D.velocity = pathDirection * (speed * Time.deltaTime);
+				yield return null;
+
+			}
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (path != null)
+		{
+			for (int i = targetIndex; i < path.Length; i++)
+			{
+				Gizmos.color = Color.black;
+				//Gizmos.DrawCube((Vector3)path[i], Vector3.one *.5f);
+
+				if (i == targetIndex)
+				{
+					Gizmos.DrawLine(transform.position, path[i]);
+				}
+				else
+				{
+					Gizmos.DrawLine(path[i - 1], path[i]);
+				}
+			}
+		}
+	}
+}
